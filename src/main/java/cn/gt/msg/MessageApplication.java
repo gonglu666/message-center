248db32d7d59e6ccf34055b370628d705b478275
gonglu <gonglu@gt.cn>
2025-11-15 19:08:13 +0800

    init project

---
+++ src/main/java/cn/gt/msg/MessageApplication.java
@@ -0,0 +1,13 @@
+package cn.gt.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageApplication.class, args);
+    }
+
+}
